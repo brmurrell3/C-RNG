2005e956948f2a9bc2009467844f99f54aa259bc
bmurrell <bmurrell@umich.edu>
2021-06-08 18:26:49 -0400

    simplified

--- UserGuess.h
+++ UserGuess.h
@@ -6,18 +6,22 @@ public:
     // Static int for the correct number we are trying to guess since it is the same for all UserGuess objects
     static int correctNum;
     
-    // Conctructor executes all code on creation of each UserGuess object
+    // Constructor executes all code on creation of each UserGuess object
     UserGuess(int guess) {
         // Initialize guess
         this->guess = guess;
         
-        // Set compare to -1 if guess is less than correctNum, 0 if equal to correctNum, 1 if greater than correctNum
+        // Set compare < 0 if guess is less than correctNum, 0 if equal to correctNum, > 0 if greater than correctNum
         compare = guess - correctNum;
     }
+
+    // Default constructor
+    UserGuess() : guess(INT_MIN), compare(INT_MIN) {}
     
     int getGuess() {
         return guess;
     }
+
     int getCompare() {
         return compare;
     }
@@ -26,6 +30,5 @@ private:
     int guess;
     int compare;
 };
-
 
 #endif /* UserGuess_h */
--- main.cpp
+++ main.cpp
@@ -1,16 +1,48 @@
 #include <iostream>
 #include <random>
-#include <string.h>
-#include "UserGuess.h"
+#include <climits>
+
+class UserGuess {
+public:
+    // Static int for the correct number we are trying to guess since it is the same for all UserGuess objects
+    static int correctNum;
+
+    // Constructor executes all code on creation of each UserGuess object
+    UserGuess(int guess) {
+        // Initialize guess
+        this->guess = guess;
+
+        // Set compare < 0 if guess is less than correctNum, 0 if equal to correctNum, > 0 if greater than correctNum
+        compare = guess - correctNum;
+    }
+
+    // Default constructor
+    UserGuess() : guess(INT_MIN), compare(INT_MIN) {}
+
+    int getGuess() {
+        return guess;
+    }
+
+    int getCompare() {
+        return compare;
+    }
+
+private:
+    int guess;
+    int compare;
+};
 
 // DO NOT EDIT, Defines functions
-int getRandomNumber(u_int, u_int);
+int getRandomNumber(int, int);
 void printGuesses(UserGuess*, int);
 void addGuess(UserGuess*, int);
 
 // Define minimum and maximum values to be generated, you may change either if range is to large
 const int MIN_VALUE = 1;
-const int MAX_VALUE = 100;
+const int MAX_VALUE = 10;
+
+// Defines a constant variable containing the maximum number of users guesses, you may change this if you'd like more or less guesses for the user
+const int MAX_GUESSES = 5;
 
 // ***DO NOT EDIT*** Generates random number for user to guess
 const int correctGuess = getRandomNumber(MIN_VALUE, MAX_VALUE);
@@ -22,20 +54,11 @@ int main(int argc, const char * argv[]) {
     
     // Print title of game
     std::cout << "*** Random Number Guessing Game ***\n";
-    
-    // Defines a constant variable containing the maximum number of users guesses, you may change this if you'd like more or less guesses for the user
-    int maxGuesses = 5;
-    
-    /*
-     Defines C style array (series of numbers) that will contain the values guessed by the user
-     sizeof(UserGuess) * maxGuesses will find the total amount of memory needed to store UserGuess maxGuesses times (in this case it will be the space to store 5 numbers)
-     malloc just tells the compiler to save this amount of space in memory because we will need it later
-     Do NOT edit the code to the left of "malloc"
-     You may edit the code to the right of "malloc" to change the size of memory needed
-    */
-    UserGuess* recordedGuesses = (UserGuess*) malloc(sizeof(UserGuess) * maxGuesses);
 
-    
+    // Defines an array (series of numbers) on the heap that will contain the values guessed by the user on the heap
+    // This will need to be deleted when finished to avoid memory leaks
+    UserGuess* recordedGuesses = new UserGuess[MAX_GUESSES];
+
     /*
      This sets the users guess to a number (-1) that is not in range of correctGuess so there is no way the number can be correct without the user guessing
      In C++, you must initialize varibales (set the variable equal to something) at some point before performing operations on it
@@ -44,16 +67,17 @@ int main(int argc, const char * argv[]) {
      int a;
      a = a + 1;
      the compiler may error because a is equal to nothing at the time an addition operator is performed
-     
-     In Java, this is not true most numerical variables are automatically set to 0 so the above code would be valid
      */
     int userInput = -1;
     
     // Defines counter varible for looping through array, arrays start at entry 0 so i is initalized to 0
     int i = 0;
+
+    // Define a flag to indicate when a correct guess has been made
+    bool correct = false;
     
     // Continue looping while the user guess is incorrect and our counter is less than the max guesses we give the user
-    while (correctGuess != userInput && i < maxGuesses) {
+    while (!correct && i < MAX_GUESSES) {
         
         // Prompt user for their guess
         std::cout << "Guess a number from " << MIN_VALUE << " to " << MAX_VALUE << ": ";
@@ -65,44 +89,31 @@ int main(int argc, const char * argv[]) {
         addGuess(&recordedGuesses[i], userInput);
         
         // If the user guessed correctly, congratulate them, print all the guesses they made and exit the program
-        if (recordedGuesses[i].getCompare() == 0) {
+        correct = (recordedGuesses[i].getCompare() == 0);
+        if (correct) {
             std::cout << "Congratulations, you guessed the correct number\n";
-            printGuesses(recordedGuesses, sizeof(*recordedGuesses));
-            
-            
-            /*
-            // Program is done, we must free the memory of dynamically allocated space
-            for(UserGuess* a = recordedGuesses; a < a + sizeof(recordedGuesses)/sizeof(*a); ++a) {
-                free(a);
-            }
-             */
-            
-            system("pause");
-            return 0;
         }
-        // The user did not guess correctly so tell them if they guessed too high or too low
         else {
+            // The user did not guess correctly so tell them if they guessed too high or too low
             if (recordedGuesses[i].getCompare() > 0){
-                std::cout << "Guess was too high. ";
+                std::cout << "Guess was too high.\n";
             }
             else {
-                std::cout << "Guess was too low. ";
+                std::cout << "Guess was too low.\n";
             }
         }
         // Increment the counter variable
         i++;
     }
     
-    std::cout << "\nYou have run out of guesses\n";
-    printGuesses(recordedGuesses, sizeof(*recordedGuesses));
+    if (!correct) {
+        std::cout << "\nYou have run out of guesses, the number was " << correctGuess << ".\n";
+    }
+    printGuesses(recordedGuesses, i);
     
-    // Program is done, we must free the memory of  allocated space
-    //for (int i = 0; i < maxGuesses; ++i) {
-     //    delete recordedGuesses[i];
-     //}
-    free(recordedGuesses);
+    // Release the memory allocated in the heap
+    delete[] recordedGuesses;
     
-    system("pause");
     return 0;
 }
 
@@ -123,7 +134,7 @@ void addGuess(UserGuess* recordedGuesses, int a) {
 
 // DO NOT EDIT CODE BELOW THIS LINE
 // Function to generate a random number with a minimum value of x and a maximum value of y
-int getRandomNumber(u_int x, u_int y) {
+int getRandomNumber(int x, int y) {
 
     // Obtain a random number from hardware
     std::random_device rand;
@@ -145,10 +156,10 @@ int getRandomNumber(u_int x, u_int y) {
 
 void printGuesses(UserGuess* recordedGuesses, int size) {
     std::cout << "Your guesses are: ";
-    
-    while(recordedGuesses->getGuess() != 0) {
-        std::cout << recordedGuesses->getGuess() << " ";
-        recordedGuesses++;
+    for (int i = 0; i < size; ++i) {
+        std::cout << recordedGuesses[i].getGuess() << " ";
     }
     std::cout << "\n";
 }
+
+
